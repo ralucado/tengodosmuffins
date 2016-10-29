5b48328fab982942bed52a7bfab072b91ade2ff5
kondorriano <nicobito@gmail.com>
2016-10-29 22:36:08 +0200

    Player tex, player collision

--- Character.cpp
+++ Character.cpp
@@ -32,26 +32,31 @@ void Character::moveWithCollisions(sf::Vector2f dist) {
     move(dist);
 }
 
+bool in_interval(float v, float lo, float hi) {
+    return v >= lo && v <= hi;
+}
+
 bool Character::collides(sf::Vector2f dist)
 {
     sf::FloatRect r = getLocalBounds();
     sf::Vector2f pos = getPosition()+dist;
     sf::Image* p = collisionMap;
     sf::Color c = sf::Color::Black;
-
-    std::vector<sf::Vector2f> points;
-    points.push_back(sf::Vector2f(r.left+pos.x, r.top+pos.y));
-    points.push_back(sf::Vector2f(r.left+r.width+pos.x, r.top+pos.y));
-    points.push_back(sf::Vector2f(r.left+pos.x, r.top+r.height+pos.y));
-    points.push_back(sf::Vector2f(r.left+r.width+pos.x, r.top+r.height+pos.y));
+    int size = 64;
+    sf::Vector2f topleft = {r.left+pos.x, r.top+pos.y};
+    std::vector<sf::Vector2f> colliders;
+    for(int i = 0; i < size; i++) {
+        colliders.push_back(topleft + sf::Vector2f(i, 0));
+        colliders.push_back(topleft + sf::Vector2f(0, i));
+        colliders.push_back(topleft + sf::Vector2f(size, i));
+        colliders.push_back(topleft + sf::Vector2f(i, size));
+    };
     float minX = 0;
     float maxX = p->getSize().x-1;
     float minY = 0;
     float maxY = p->getSize().y-1;
-    for(auto& pt : points) {
-        pt.x = clamp(pt.x, minX, maxX);
-        pt.y = clamp(pt.y, minY, maxY);
-        if(p->getPixel(pt.x, pt.y) == c)
+    for(auto& pt : colliders) {
+        if(in_interval(pt.x, minX, maxX) && in_interval(pt.y, minY, maxY) && p->getPixel(pt.x, pt.y) == c)
             return true;
     }
     return false;
